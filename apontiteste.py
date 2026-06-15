20efe8e3d0698b2e262be36d95e3217089f1ee40
MatheusWolf <45229807+MatheusWolf@users.noreply.github.com>
2026-06-15 18:59:29 +0000

    adicionado inputs de nome e leitor de numeros mais função somatoria

---
+++ apontiteste.py
@@ -0,0 +1,11 @@
+print("Opa tudo bem")
+
+nome = input("Digite seu nome")
+print("Ola, ", nome)
+
+num1 = int(input("num 1: "))
+num2 = int(input("num 2: "))
+
+soma = num1 + num2
+
+print("A soma dos numeros é:", soma)
